d0378b9429d07fc6e61afc412edad119b29158f1
egolubev <135797531>
2022-01-12 22:39:06 +0300

    Human Readable Time

---
+++ 5/human_readable_time.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Human Readable Time
+ * User: egolubev
+ * Date: 12.10.2021
+ * Time: 23:27
+ * @param $seconds
+ * @return string
+ */
+
+function human_readable($seconds) {
+    $t = $seconds;
+    $h = floor($t / 3600);
+    $m = floor(($t - $h * 3600) / 60);
+    $s = $t - $h * 3600 - $m * 60;
+    return sprintf('%02d:%02d:%02d', $h, $m, $s);
+}
